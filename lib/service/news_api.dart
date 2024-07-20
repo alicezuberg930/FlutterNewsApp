c37d26634b8fb061907bdd2dab031a83ce787cee
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-07-20 12:11:02 +0700

    fix api

--- lib/service/news_api.dart
+++ lib/service/news_api.dart
@@ -7,7 +7,7 @@ import 'package:news_app/service/data.dart';
 
 class NewsAPI {
   Future<List<Article>> getAllArticles(String option, String query) async {
-    return FakeData.data.map((dynamic item) => Article.fromJson(item)).toList();
+    // return FakeData.data.map((dynamic item) => Article.fromJson(item)).toList();
     Response res = await get(Uri.parse("https://newsapi.org/v2/$option?apiKey=${Constants.apiKey}$query"));
     if (res.statusCode == 200) {
       Map<String, dynamic> body = jsonDecode(res.body);
