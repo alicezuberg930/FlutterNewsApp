9069a91985ac06e1c6d57d3c23c72ce479844919
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-09-02 00:15:39 +0700

    fix bug

--- lib/service/news_api.dart
+++ lib/service/news_api.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:news_app/common/constants.dart';
 import 'package:news_app/model/article.dart';
-import 'package:news_app/service/data.dart';
 
 class NewsAPI {
   Future<List<Article>> getAllArticles(String option, String query) async {
     // return FakeData.data.map((dynamic item) => Article.fromJson(item)).toList();
-    Response res = await get(Uri.parse("https://newsapi.org/v2/$option?apiKey=${Constants.apiKey}$query"));
+    final res = await http.get(Uri.parse("https://newsapi.org/v2/$option?apiKey=${Constants.apiKey}$query"));
     if (res.statusCode == 200) {
       Map<String, dynamic> body = jsonDecode(res.body);
       List<dynamic> dynaRes = body['articles'];
